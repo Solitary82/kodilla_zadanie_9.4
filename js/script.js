e75ca40bddb7b010fad8228ec5554730c31fd832
Marta Waplak <mwaplak@gmail.com>
2018-02-05 20:33:33 +0100

    Fix formatting and code in .js file

--- js/script.js
+++ js/script.js
@@ -1,28 +1,20 @@
 function drawTree(height) {
     
-"use strict";
+    "use strict";
     
-for(var i = 1; i < 6; i++){
-    var star = "";
-    
-    for(var j = i; j < 6; j++){
-        star += " ";
+    for(var i = 1; i < 6; i++){
+        var star = "";
+            for(var j = i; j < 6; j++){
+                star += " ";
+            } 
+            for(var k = 0; k < (i * 2) - 1; k++){
+                star += "*";
+            } 
+            if (i === height + 1) {
+                break; 
+            }
+        console.log(star); 
     }
-  
-    for(var k = 0; k < (i * 2) - 1; k++){
-        star += "*";
-    }
-    
-    if (i === height + 1) {
-    break; 
-    
-    }
-    
-    console.log(star);
-    
-    }
-
 }
 
-var xmasTree = drawTree(5);
-console.log = xmasTree;
+drawTree(5);
